38456e8d825f0eb7590a7e678018518dd4225089
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-01-19 14:08:19 -0700

    changed back to context

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import Favorite from "./Favorite";
 import Button from "./Button";
 import Header from "./Header";
 import styles from "./styles.css";
-import { ThemeContextConsumer } from "./themeContext";
 
-function App() {
+function App(props) {
   return (
-    <ThemeContextConsumer>
-      {context => (
-        <div className={`${context.theme}-theme`}>
-          <Menu />
-          <hr />
-          <Favorite />
-          <hr />
-          <Header />
-          <Button />
-        </div>
-      )}
-    </ThemeContextConsumer>
+    <div className="{props.theme}-theme">
+      <Menu />
+      <hr />
+      <Favorite />
+      <hr />
+      <Header />
+      <Button />
+    </div>
   );
 }
 
